dc7e11a71a53240677721186331515df85c25d16
Terry Tettey <35869292+MonoTerry@users.noreply.github.com>
2021-09-08 00:28:52 +0000

    Dropdown menu?

--- react-side/src/components/Header/Header.js
+++ react-side/src/components/Header/Header.js
@@ -4,30 +4,45 @@ import PropTypes from 'prop-types';
 import "./Header.scss"
 
 export const Header = ({ }) => {
+
+    function fileButtonClicked(event) {
+        document.getElementById("file-dropdown").classList.toggle("show");
+    }
+
     return (
         <div className="header-container">
             <div id="logo">
                 <h1>GBlox</h1>
             </div>
             <div className="header-button-container">
-                <button type="button" id="file-button" className="button-no-border glow">
-                    <svg id="Component_3_1" data-name="Component 3 – 1" xmlns="http://www.w3.org/2000/svg" width="120" height="40" viewBox="0 0 120 40">
-                        <path id="Path_33" data-name="Path 33" d="M306.954,42h94.091c5.06,5.1,7.9,7.961,12.955,13.063V82H294V55.063Z" transform="translate(-294 -42)" fill="#0000dc" />
-                        <g id="Group_31" data-name="Group 31" transform="translate(30.99 8.794)">
-                            <text id="File" transform="translate(18.237 17.801)" fill="#fff" font-size="17" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">File</tspan></text>
-                            <g id="Group_424" data-name="Group 424" transform="translate(-4 -2.007)">
-                                <g id="Group_30" data-name="Group 30" transform="translate(-9.427 4.252)">
-                                    <path id="Path_291" data-name="Path 291" d="M.573,1.451h6.5L9.17,3.514H18.6l1.81,2.435V18.564H.573Z" transform="translate(-0.573 -1.451)" fill="none" stroke="#fff" stroke-width="2" />
-                                </g>
-                                <path id="Path_292" data-name="Path 292" d="M10.157,9.272H3.734L1.982,11.389H-9.589" transform="translate(0 -0.16)" fill="none" stroke="#fff" stroke-width="2" />
-                                <g id="Ellipse_9" data-name="Ellipse 9" transform="translate(4.433 15.795)" fill="none" stroke="#fff" stroke-width="2">
-                                    <circle cx="1.209" cy="1.209" r="1.209" stroke="none" />
-                                    <circle cx="1.209" cy="1.209" r="0.209" fill="none" />
+                <div className="dropdown-button">
+                    <button type="button" id="file-button" className="button-no-border glow" onClick={fileButtonClicked}>
+                        <svg id="Component_3_1" data-name="Component 3 – 1" xmlns="http://www.w3.org/2000/svg" width="120" height="40" viewBox="0 0 120 40">
+                            <path id="Path_33" data-name="Path 33" d="M306.954,42h94.091c5.06,5.1,7.9,7.961,12.955,13.063V82H294V55.063Z" transform="translate(-294 -42)" fill="#0000dc" />
+                            <g id="Group_31" data-name="Group 31" transform="translate(30.99 8.794)">
+                                <text id="File" transform="translate(18.237 17.801)" fill="#fff" font-size="17" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">File</tspan></text>
+                                <g id="Group_424" data-name="Group 424" transform="translate(-4 -2.007)">
+                                    <g id="Group_30" data-name="Group 30" transform="translate(-9.427 4.252)">
+                                        <path id="Path_291" data-name="Path 291" d="M.573,1.451h6.5L9.17,3.514H18.6l1.81,2.435V18.564H.573Z" transform="translate(-0.573 -1.451)" fill="none" stroke="#fff" stroke-width="2" />
+                                    </g>
+                                    <path id="Path_292" data-name="Path 292" d="M10.157,9.272H3.734L1.982,11.389H-9.589" transform="translate(0 -0.16)" fill="none" stroke="#fff" stroke-width="2" />
+                                    <g id="Ellipse_9" data-name="Ellipse 9" transform="translate(4.433 15.795)" fill="none" stroke="#fff" stroke-width="2">
+                                        <circle cx="1.209" cy="1.209" r="1.209" stroke="none" />
+                                        <circle cx="1.209" cy="1.209" r="0.209" fill="none" />
+                                    </g>
                                 </g>
                             </g>
-                        </g>
-                    </svg>
-                </button>
+                        </svg>
+                    </button>
+                    <div className="dropdown-content" id="file-dropdown">
+                        <button className="button-no-border">New</button>
+                        <button className="button-no-border">Open</button>
+                        <button className="button-no-border">Save</button>
+                        <button className="button-no-border">Save As</button>
+                        <button className="button-no-border">Share</button>
+                        <button className="button-no-border">Close</button>
+                    </div>
+                </div>
                 <button type="button" id="edit-button" className="button-no-border glow">
                     <svg id="Component_3_2" data-name="Component 3 – 2" xmlns="http://www.w3.org/2000/svg" width="120" height="40" viewBox="0 0 120 40">
                         <path id="Path_33" data-name="Path 33" d="M306.954,42h94.091c5.06,5.1,7.9,7.961,12.955,13.063V82H294V55.063Z" transform="translate(-294 -42)" fill="#0000dc" />
@@ -40,7 +55,6 @@ export const Header = ({ }) => {
                             <path id="Path_296" data-name="Path 296" d="M3.636,3.636l-.909-.909L0,0" transform="translate(735.451 430.793)" fill="#fff" stroke="#fff" stroke-width="2" />
                         </g>
                     </svg>
-
                 </button>
                 <button type="button" id="save-button" className="button-no-border glow">
                     <svg id="Component_14_1" data-name="Component 14 – 1" xmlns="http://www.w3.org/2000/svg" width="103.381" height="40" viewBox="0 0 103.381 40">
@@ -116,7 +130,7 @@ export const Header = ({ }) => {
 
                     </button>
                 </div>
-                <button type="button" id="menu-button" className="button-no-border glow grow">
+                <button type="button" id="menu-button" className="button-no-border grow">
                     <svg id="Component_8_1" data-name="Component 8 – 1" xmlns="http://www.w3.org/2000/svg" width="48.476" height="40.339" viewBox="0 0 48.476 40.339">
                         <path id="Path_246" data-name="Path 246" d="M0,0H48.476V5.008H0Z" fill="#e9e9ff" />
                         <path id="Path_247" data-name="Path 247" d="M0,0H48.476V5.008H0Z" transform="translate(0 17.666)" fill="#e9e9ff" />
