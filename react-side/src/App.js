cde392d9a95050c1039c1d47cc6f9f82e72fc3ba
Michael Nimdea-Gyan Adu <michaelngadu@gmail.com>
2021-09-14 13:16:06 +0000

    Device Manager Attempt 2

--- react-side/src/App.js
+++ react-side/src/App.js
@@ -1,5 +1,5 @@
 import Blockly from "blockly";
-import React, { useState, useEffect, forceUpdate } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -33,25 +33,17 @@ import PropTypes from 'prop-types';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-import "./customblocks/customblocks";
-// import "./customblocks/ntypeblocks";
-import "./customblocks/compiler/arduino_core";
-import "./customblocks/peripherals/arduino_peripheral"
+import importblocks from "./customblocks/import"
+
 import './App.css';
-import "./customblocks/MelloBlocks"
-import "./customblocks/MelloBlocksGen"
 import "./blocklyextras/custom_category"
 import "./blocklyextras/toolbox_style.css"
 
-<<<<<<< Updated upstream
-import { Header } from "./components/Header/Header"
-import { Body } from "./components/Body/Body"
-=======
 import Header from "./components/Header"
 import Body from "./components/Body"
 import HeaderButton from "./components/HeaderButton";
->>>>>>> Stashed changes
 
+import { DeviceList } from "./deviceDef/device_list.js"
 import { toolboxCategories, newToolBox, MelloToolbox, MelloDOM } from "./customblocks/toolboxes/toolboxes"
 import { mainLoopCode } from "./customblocks/compiler/arduino_core"
 const { ipcRenderer } = window.require('electron')
@@ -63,6 +55,8 @@ var currentToolboxName = "Mello";
 var variables_created = [];
 var OurWorkspace;
 var workspaces = [undefined];
+var current_device = `No Device Selected`;
+
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -186,13 +180,27 @@ var test_theme = Blockly.Theme.defineTheme('test_theme', {
   'startHats': true
 });
 
-const mello_laxml = `<xml xmlns="https://developers.google.com/blockly/xml"><block type="m_mainloop" x="430" y="150"></block></xml>`;
-if (currentToolboxName == "Mello") {
-  var newxml = mello_laxml;
-  var newxmldom = Blockly.Xml.textToDom(newxml);
-}
+var default_workspace = `<xml xmlns="https://developers.google.com/blockly/xml"></xml>`;
+var newxml = default_workspace;
+var newxmldom = Blockly.Xml.textToDom(newxml);
+
 var newWorkspaceTab = 0;
-const WorkspaceTabs = () => {
+
+function App() {
+
+  const [javascriptcode, setJavascriptCode] = useState("");
+  const [upload_status, setUploadStatus] = useState("");
+  const [tabpanelval, settabpanel] = useState(0);
+  const [UploadProgress, setUploadProgress] = useState(1);
+  const classes = useStyles();
+  const [workspace, setWorkspace] = useState(``);
+  const [serialport_monitor, setSerialPortMonitor] = useState([]);
+  const [serialport_value, setSerialPortWrite] = useState("");
+  const [newvariable_name, setNewVariableName] = useState("");
+  const [newvariable_type, setNewVariableType] = useState("float");
+  const [dialog_open, setDialogOpen] = useState(false);
+  const [device_dialog_open, setDeviceDialogOpen] = useState(true);
+  const [device_chosen, setDeviceChosen] = useState("");
 
   const [workspacebuttons, setWorkspacebuttons] = useState([<div id={`Tab ${1}`}>
     <button id={`workspace_${1}`} onClick={(event, reason) => {
@@ -206,157 +214,62 @@ const WorkspaceTabs = () => {
     //Save old Workspace
     workspaces[newWorkspaceTab] = currentWorkspace;
     newWorkspaceTab = event.currentTarget.value - 1;
-    console.log(workspaceTab)
     //Load new Workspace
     if (workspaces[newWorkspaceTab] !== undefined || null) {
       OurWorkspace.clear();
       Blockly.Xml.domToWorkspace(workspaces[newWorkspaceTab], OurWorkspace);
-      console.log("Loaded workspace")
     }
     else {
       OurWorkspace.clear();
       workspaces[newWorkspaceTab] = Blockly.Xml.workspaceToDom(OurWorkspace);
-      Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(mello_laxml), OurWorkspace);
-      console.log("New Workspace Save");
+      Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(default_workspace), OurWorkspace);
     }
   }
 
   const deleteTab = (event) => {
-    console.log(`Deleting Tab ${event.currentTarget.value}`)
     var tab = event.currentTarget.value - 1;
     setWorkspacebuttons(workspacebuttons => workspacebuttons.slice(0, tab).concat(workspacebuttons.slice(tab + 1)));
     workspaces.splice(event.currentTarget.value - 1, 1)
-    console.log("Deleted Workspace")
-    console.log(workspaces)
-    try{
-    var button = document.getElementById(`workspace_${tab+1}`)
-    button.click()
-    }
-    catch(e){}
+    // try{
+    // var button = document.getElementById(`workspace_${tab+1}`)
+    // button.click()
+    // }
+    // catch(e){}
   }
-
-  useEffect(() => {
-    try{
-    var next_tab = document.getElementById(`workspaceTabs`).children
-    console.log(next_tab)
-    for (var i = 0; i < workspaces.length-1; i++) {
-      next_tab[i].id=`Tab ${i+1}`;
-      next_tab[i].children[0].id = `workspace_${i+1}`
-      next_tab[i].children[0].innerHTML = `Workspace ${i+1}`
-      next_tab[i].children[1].id = `Close Button ${i+1}`
-      next_tab[i].children[1].value=i+1
-    }
-  }
-  catch(e){}
-  })
 
   const addworkspaceTab = event => {
     var currenttab = workspacebuttons.length + 1;
     if (document.getElementById(`Tab ${workspacebuttons.length + 1}`) === null) {
-      console.log("New Workspace Created");
       setWorkspacebuttons(workspacebuttons.concat(
         <div id={`Tab ${workspacebuttons.length + 1}`}>
           <button id={`workspace_${workspacebuttons.length + 1}`} onClick={(event, reason) => {
             workspaceTabchange(event);
             setWorkspaceTab(event.currentTarget.value - 1);
-            console.log(workspaceTab)
           }} value={currenttab}>{`Workspace ${workspacebuttons.length + 1}`}</button>
           <button id={`Close Button ${workspacebuttons.length + 1}`} value={currenttab} onClick={(event, reason) => {
             deleteTab(event);
           }}>x</button></div>));
-      console.log(workspacebuttons)
       workspaces.push(undefined)
     }
     else {
-      console.log("New Workspace Created");
       currenttab += 1;
       setWorkspacebuttons(workspacebuttons.concat(
         <div id={`Tab ${workspacebuttons.length + 2}`}>
           <button id={`workspace_${workspacebuttons.length + 2}`} onClick={(event, reason) => {
             workspaceTabchange(event);
             setWorkspaceTab(event.currentTarget.value - 1);
-            console.log(workspaceTab)
           }} value={currenttab + 1}>{`Workspace ${workspacebuttons.length + 2}`}</button>
           <button id={`Close Button ${workspacebuttons.length + 2}`} value={currenttab + 1} onClick={(event, reason) => {
             deleteTab(event);
           }}>x</button></div>));
-      console.log(workspacebuttons)
       workspaces.push(undefined)
     }
 
   };
-  return (
-    <div id={`workspaceTabs`}>
-      {workspacebuttons}
-      <Button onClick={addworkspaceTab}>Add Workspace Tab</Button>
-    </div>
-  )
-}
-
-function App() {
-
-  /*
-  Blockly.Themes.FutureRetro = Blockly.Theme.defineTheme('futureRetro', 
-  {
-    'blockStyles': {
-      "colourPrimary": "#4a148c",
-      "colourSecondary":"#AD7BE9",
-      "colourTertiary":"#CDB6E9"
-    },
-    'categoryStyles':{
-      "colour": "#4a148c"
-    },
-    'componentStyles':{
-      'workspaceBackgroundColour': '#1e1e1e'
-    },
-  });
-  */
-
-  const [javascriptcode, setJavascriptCode] = useState("");
-  const [upload_status, setUploadStatus] = useState("");
-  const [tabpanelval, settabpanel] = useState(0);
-  const [toolbox_used, setToolboxUsed] = useState(1);
-  const [UploadProgress, setUploadProgress] = useState(1);
-  const classes = useStyles();
-  const [serialport_monitor, setSerialPortMonitor] = useState([]);
-  const [serialport_value, setSerialPortWrite] = useState("");
-  const [newvariable_name, setNewVariableName] = useState("");
-  const [newvariable_type, setNewVariableType] = useState("float");
-  const [dialog_open, setDialogOpen] = useState(false);
-
-
 
   const code_change = event => {
     setJavascriptCode(event.target.value);
   }
-
-  const select_toolbox = event => {
-    setToolboxUsed(event.target.value);;
-    switch (event.target.value) {
-      case 1:
-        currentToolbox = MelloDOM;
-        currentToolboxName = "Mello";
-
-        break;
-      case 2:
-        currentToolbox = newToolBox;
-        currentToolboxName = "Basic";
-        break;
-      case 3:
-        currentToolbox = toolboxCategories;
-        currentToolboxName = "Advanced"
-        break;
-    }
-    console.log("switch?")
-    if (OurWorkspace !== null && OurWorkspace !== undefined) {
-      console.log("Ye")
-      OurWorkspace.updateToolbox(currentToolbox);
-      if (currentToolboxName === "Mello") {
-        Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(mello_laxml), OurWorkspace);
-      }
-    }
-  }
-
 
   const tabpanelchange = (event, newTabval) => {
     const oldTabval = tabpanelval;
@@ -405,7 +318,7 @@ function App() {
     if (currentToolboxName === "Mello" || currentToolboxName === "Basic") {
       code = mainLoopCode;
     }
-    Blockly.mainWorkspace.registerButtonCallback("createvar", logbutton)
+    OurWorkspace.registerButtonCallback("createvar", logbutton)
     newxmldom = Blockly.Xml.workspaceToDom(workspace);
     newxml = Blockly.Xml.domToText(newxmldom);
     if (tabpanelval === 0) {
@@ -414,6 +327,7 @@ function App() {
       Blockly.Xml.domToWorkspace(newxmldom, workspace);
     }
     setJavascriptCode(code);
+    importblocks.importblocks();
   }
 
   // Drawer Stuff
@@ -426,7 +340,7 @@ function App() {
   function clearWorkspace() {
     Blockly.mainWorkspace.clear();
     if (currentToolboxName === "Mello" || currentToolboxName === "Basic") {
-      Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(mello_laxml), Blockly.mainWorkspace);
+      Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(default_workspace), Blockly.mainWorkspace);
     }
   }
 
@@ -516,7 +430,7 @@ function App() {
         }
       })
       OurWorkspace.registerButtonCallback("Openfly", function (event) {
-        OurWorkspace.toolbox_.flyout_.show(mello_laxml)
+        OurWorkspace.toolbox_.flyout_.show(default_workspace)
       });
 
     }
@@ -538,16 +452,50 @@ function App() {
       OurWorkspace.toolbox_.flyout_.positionAt_(500,500,0,0)
       LogicButton.click();
       */
+      
+      //console.log(Blockly.utils.toolbox.convertToolboxDefToJson(MelloToolbox))
       /*
-      console.log(Blockly.utils.toolbox.convertToolboxDefToJson(MelloToolbox))
       OurWorkspace.refreshToolboxSelection()
       OurWorkspace.toolbox_.flyout_.show(Blockly.Xml.textToDom(`<xml> <block type="variable_get"></block> </xml>`))
       */
     }
   }
 
-  React.useEffect(() => {
+  function closedevicemanager() {
+    var chosen_device_list = DeviceList.findIndex(o => o.device_name === device_chosen)
+    default_workspace = DeviceList[chosen_device_list].default_workspace;
+    current_device = DeviceList[chosen_device_list].device_name;
+    if (current_device.includes("Arduino")==0){
+      currentToolbox = Blockly.utils.toolbox.convertToolboxDefToJson(DeviceList[chosen_device_list].toolbox)
+    }
+    else{
+      currentToolbox = DeviceList[chosen_device_list].toolbox;
+    }
+    OurWorkspace.updateToolbox(currentToolbox);
+    console.log(currentToolbox);
+    console.log(current_device);
+    if (newxml === `<xml xmlns="https://developers.google.com/blockly/xml"></xml>`) {
+      Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(default_workspace), Blockly.mainWorkspace);
+    }
+    setDeviceDialogOpen(false);
+  }
+  const setDevice = (event) => {
+    setDeviceChosen(event.target.value)
+    var chosen_device = event.target.value;
 
+  }
+  React.useEffect(() => {
+    try {
+      var next_tab = document.getElementById(`workspaceTabs`).children
+      for (var i = 0; i < workspaces.length - 1; i++) {
+        next_tab[i].id = `Tab ${i + 1}`;
+        next_tab[i].children[0].id = `workspace_${i + 1}`
+        next_tab[i].children[0].innerHTML = `Workspace ${i + 1}`
+        next_tab[i].children[1].id = `Close Button ${i + 1}`
+        next_tab[i].children[1].value = i + 1
+      }
+    }
+    catch (e) { }
     if (upload_status === "No Arduino Detected" || upload_status === "Upload Successful" || upload_status.includes("Upload Failed : Error in Code") === 1 || upload_status === "") {
       setUploadProgress(1);
     }
@@ -559,7 +507,7 @@ function App() {
 
   return (
     <div className="App">
-      <HeaderButton color="#FF0000"  some="Hello World"/>
+      <HeaderButton color="#FF0000" some="Hello World" />
       <Header />
       <Body></Body>
       <div id="ToolboxHolder"></div>
@@ -592,39 +540,33 @@ function App() {
             Mintduino
           </Typography>
           <Button onClick={sendflyout}>TesstTool</Button>
+          <Button onClick={(event) => setDeviceDialogOpen(true)}>Device Manager</Button>
           {UploadProgress == 0 &&
             <CircularProgress color="secondary" />
           }
           <Button color="inherit" onClick={uploadCode_ipc}>Upload</Button>
-          {/* <FormGroup>
-            <FormControlLabel
-              color="inherit" control={
-                <Switch
-                  checked={toolboxstate}
-                  onChange={toolboxchange}
-                  onClick={toolboxchange}
-                  value={toolboxstate}
-                  name="toolbox"
-                />
+          <div id={"Device Manager Div"}>
+            <Dialog
+              onClose={(event, reason) => {
+                if (reason == 'backdropClick') {
+                  setDeviceDialogOpen(false);
+                }
               }
-              labelPlacement="start"
-              label="Advanced Toolbox"
-            />
-          </FormGroup> */}
-          <FormControl>
-            <InputLabel id="Toolbox Select">Toolbox</InputLabel>
-            <Select
-              labelId="Toolbox Select"
-              id="Toolbox Select"
-              value={toolbox_used}
-              variant="outlined"
-              onChange={select_toolbox}
-            >
-              <MenuItem value={1}>Mello Toolbox</MenuItem>
-              <MenuItem value={2}>Basic Toolbox</MenuItem>
-              <MenuItem value={3}>Advanced Toolbox</MenuItem>
-            </Select>
-          </FormControl>
+              } open={device_dialog_open}>
+              <DialogTitle id="simple-dialog-title">Choose Device</DialogTitle>
+              <Select
+                value={device_chosen}
+                variant="outlined"
+                onChange={setDevice}
+              >
+                <MenuItem value={"Arduino Uno"}>Arduino Uno</MenuItem>
+                <MenuItem value={"Arduino Nano"}>Arduino Nano</MenuItem>
+                <MenuItem value={"Mello"}>Mello</MenuItem>
+                <MenuItem value={"Mingo"}>Mingo</MenuItem>
+              </Select>
+              <Button onClick={closedevicemanager}>Ok</Button>
+            </Dialog>
+          </div>
         </Toolbar>
       </AppBar>
       <div>
@@ -637,7 +579,10 @@ function App() {
         <TabPanel value={tabpanelval} index={0} className={classes.Tabs}>
           <section id="blocklyArea">
             <div id="blocklyDiv">
-              <WorkspaceTabs />
+              <div id={`workspaceTabs`}>
+                {workspacebuttons}
+                <Button onClick={addworkspaceTab}>Add Workspace Tab</Button>
+              </div>
               <Dialog
                 onClose={(event, reason) => {
                   if (reason == 'backdropClick') {
