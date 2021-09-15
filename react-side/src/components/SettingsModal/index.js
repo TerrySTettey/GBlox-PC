141e949d9932a119b31d035e3fbe8c1c96e50139
Terry Tettey <35869292+MonoTerry@users.noreply.github.com>
2021-09-15 11:26:37 +0000

    Pushing Font Style to storybook

--- react-side/src/components/SettingsModal/index.js
+++ react-side/src/components/SettingsModal/index.js
@@ -1,24 +1,18 @@
 import React, { useState, useRef, useEffect } from 'react'
-import PropTypes from "prop-types"
 
-import HeaderButton from "./../HeaderButton"
+import "./SettingsModal.scss"
 
-import "./Dropdown.scss"
-
-const Dropdown = (props) => {
+const SettingsModal = (props) => {
     var [buttonState, setButtonState] = useState("Out");
-    const dropDownContainer = useRef(null);
-    const dropdownBox = useRef(null)
-    useOutsideAlerter(dropDownContainer)
 
-    // On click outside of dropbox
+    // On click outside of Object
     function useOutsideAlerter(ref) {
         useEffect(() => {
             //Alert on click outside of element
             function handleClickOutside(event) {
                 if (ref.current && !ref.current.contains(event.target)) {
                     setButtonState("Out");
-                    dropdownBox.current.style.display = "none"
+                    //dropdownBox.current.style.display = "none"
                 }
             }
 
@@ -29,50 +23,35 @@ const Dropdown = (props) => {
         }, [ref])
     }
 
-    function buttonClicked() {
-        if (buttonState === "Out") {
-            setButtonState("In");
-            dropdownBox.current.style.display = "block"
-        } else if (buttonState === "In") {
-            setButtonState("Out");
-            dropdownBox.current.style.display = "none"
-        }
-        console.log(dropdownBox)
-
-    }
-
-    var ButtonHolder = [];
-    function populateDropbox() {
-        var i = 0;
-        if (props.list !== undefined && props.funcsOnClick !== undefined) {
-            for (i; i < props.list.length; i++) {
-                ButtonHolder.push(<button className="button-no-border" onClick={props.funcsOnClick[i]} >{props.list[i]}</button>)
-            }
-        }
-    }
-
     return (
-        <div className="dropdown-container" ref={dropDownContainer}>
-            <HeaderButton buttonImage={props.buttonImage} onClick={buttonClicked} s_ButtonState={buttonState} />
-            <div className="blue-dropdown-box" ref={dropdownBox}>
-                {populateDropbox()}
-                {ButtonHolder}
+        <div className="modal-container">
+            <div className="modal-section">
+                <h1>Language</h1>
             </div>
+            <div className="modal-section-break" />
+            <div className="modal-section">
+                <h1>Themes</h1>
+                <div style={{}}>
+                    <div style={{}}>
+                    <p>Dark Mode</p>
+                    <div className="modal-lighting-buttons"/>
+                    </div>
+                    <div style={{}}>
+                    <p>Light Mode</p>
+                    </div>
+                </div>
+            </div>
+            
+
+            <div className="modal-section-break" />
         </div>
     )
 }
 
-Dropdown.defaultProps = {
-    buttonImage: 5,
-    list: ["Hello", "World"],
-    funcsOnClick: [() => { console.log("Hello") }, () => { console.log("World") }],
-    type: "2"
+SettingsModal.defaultProps = {
 }
 
-Dropdown.propTypes = {
-    buttonImage: PropTypes.string,
-    list: PropTypes.arrayOf(PropTypes.string),
-    funcs: PropTypes.arrayOf(PropTypes.func)
+SettingsModal.propTypes = {
 }
 
-export default Dropdown
+export default SettingsModal
