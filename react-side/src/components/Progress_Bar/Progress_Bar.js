e4f013bef46f106ae0f433a498bad2d1746ec92e
Michael Nimdea-Gyan Adu <michaelngadu@gmail.com>
2021-09-15 13:52:06 +0000

    Temp Svg Commit

---
+++ react-side/src/components/Progress_Bar/Progress_Bar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Progress_Bar(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Progress_Bar
+
