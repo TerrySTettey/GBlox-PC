c2934e4d573410e8a3b2b186735d17251d6f4c0c
Michael Nimdea-Gyan Adu <michaelngadu@gmail.com>
2021-09-17 09:42:47 +0000

    Updated Progress Bar and Upload Circle

--- react-side/src/components/Upload_Circle/Upload_Circle.stories.js
+++ react-side/src/components/Upload_Circle/Upload_Circle.stories.js
@@ -4,17 +4,9 @@ import Upload_Circle from './Upload_Circle'
 import './Upload_Circle.scss'
 
 function Upload_Circle_Component() {
-    const get_origin = event => {
-        var test = document.getElementById("Circle SVG");
-        var origin = window.getComputedStyle(test, null).getPropertyValue("transform-origin");
-        var inner = document.getElementById("Inner_Circle")
-        console.log(window.getComputedStyle(inner, null).getPropertyValue("transform-origin"));
-    }
-
     return (
         <Upload_Circle>
         </Upload_Circle>
-
     )
 }
 
