234a0ef827fdc5bfa883270d42753e9e6dde84f3
Terry Tettey <35869292+MonoTerry@users.noreply.github.com>
2021-09-22 15:11:20 +0000

    Toolbox line done

--- react-side/src/components/Toolbox/Toolbox.js
+++ react-side/src/components/Toolbox/Toolbox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import PropTypes from 'prop-types'
 import './Toolbox.scss'
 import Button from '../Button'
@@ -43,25 +43,177 @@ function level_group(level) {
     }
     return button_levels;
 }
+
+
+
 function Toolbox(props) {
     const { children } = props;
+    var Connector = useRef(null);
     var level = "Beginner"
+
+    function level1OnClick() {
+        if(Connector.current.classList.contains("con-pos-1")){
+            Connector.current.classList.remove("con-pos-1");
+        }
+        if(Connector.current.classList.contains("con-pos-2")){
+            Connector.current.classList.remove("con-pos-2");
+        }
+        if(Connector.current.classList.contains("con-pos-3")){
+            Connector.current.classList.remove("con-pos-3");
+        }
+        if(Connector.current.classList.contains("con-pos-4")){
+            Connector.current.classList.remove("con-pos-4");
+        }
+        if(Connector.current.classList.contains("con-pos-5")){
+            Connector.current.classList.remove("con-pos-5");
+        }
+
+        Connector.current.classList.add("con-pos-1");
+    }
+
+    function level2OnClick() {
+        if(Connector.current.classList.contains("con-pos-1")){
+            Connector.current.classList.remove("con-pos-1");
+        }
+        if(Connector.current.classList.contains("con-pos-2")){
+            Connector.current.classList.remove("con-pos-2");
+        }
+        if(Connector.current.classList.contains("con-pos-3")){
+            Connector.current.classList.remove("con-pos-3");
+        }
+        if(Connector.current.classList.contains("con-pos-4")){
+            Connector.current.classList.remove("con-pos-4");
+        }
+        if(Connector.current.classList.contains("con-pos-5")){
+            Connector.current.classList.remove("con-pos-5");
+        }
+
+        Connector.current.classList.add("con-pos-2");
+    }
+    function level3OnClick() {
+        if(Connector.current.classList.contains("con-pos-1")){
+            Connector.current.classList.remove("con-pos-1");
+        }
+        if(Connector.current.classList.contains("con-pos-2")){
+            Connector.current.classList.remove("con-pos-2");
+        }
+        if(Connector.current.classList.contains("con-pos-3")){
+            Connector.current.classList.remove("con-pos-3");
+        }
+        if(Connector.current.classList.contains("con-pos-4")){
+            Connector.current.classList.remove("con-pos-4");
+        }
+        if(Connector.current.classList.contains("con-pos-5")){
+            Connector.current.classList.remove("con-pos-5");
+        }
+
+        Connector.current.classList.add("con-pos-3");
+    }
+    function level4OnClick() {
+        if(Connector.current.classList.contains("con-pos-1")){
+            Connector.current.classList.remove("con-pos-1");
+        }
+        if(Connector.current.classList.contains("con-pos-2")){
+            Connector.current.classList.remove("con-pos-2");
+        }
+        if(Connector.current.classList.contains("con-pos-3")){
+            Connector.current.classList.remove("con-pos-3");
+        }
+        if(Connector.current.classList.contains("con-pos-4")){
+            Connector.current.classList.remove("con-pos-4");
+        }
+        if(Connector.current.classList.contains("con-pos-5")){
+            Connector.current.classList.remove("con-pos-5");
+        }
+
+        Connector.current.classList.add("con-pos-4");
+    }
+    function level5OnClick() {
+        if(Connector.current.classList.contains("con-pos-1")){
+            Connector.current.classList.remove("con-pos-1");
+        }
+        if(Connector.current.classList.contains("con-pos-2")){
+            Connector.current.classList.remove("con-pos-2");
+        }
+        if(Connector.current.classList.contains("con-pos-3")){
+            Connector.current.classList.remove("con-pos-3");
+        }
+        if(Connector.current.classList.contains("con-pos-4")){
+            Connector.current.classList.remove("con-pos-4");
+        }
+        if(Connector.current.classList.contains("con-pos-5")){
+            Connector.current.classList.remove("con-pos-5");
+        }
+
+        Connector.current.classList.add("con-pos-5");
+    }
+
+
+
     return (
         <div className="Toolbox">
             <div className="toolbox-title">Level: {level}</div>
             <div className="c-buttongroup-a-Levels">
-                {level_group(5)}
+                {/*level_group(5)*/}
+                <Button
+                    type="ToolboxLevelButton"
+                    s_ButtonState="Out"
+                    inColor="#060841"
+                    outColor="#060841"
+                    hoverColor="#0000DC"
+                    text={1}
+                    hoverEffect="fill"
+                    onClick={level1OnClick}>
+                </Button>
+                <div className="level-connector"></div>
+                <Button
+                    type="ToolboxLevelButton"
+                    s_ButtonState="Out"
+                    inColor="#060841"
+                    outColor="#060841"
+                    hoverColor="#0000DC"
+                    text={2}
+                    hoverEffect="fill"
+                    onClick={level2OnClick}>
+                </Button>
+                <div className="level-connector"></div>
+                <Button
+                    type="ToolboxLevelButton"
+                    s_ButtonState="Out"
+                    inColor="#060841"
+                    outColor="#060841"
+                    hoverColor="#0000DC"
+                    text={3}
+                    hoverEffect="fill"
+                    onClick={level3OnClick}>
+                </Button>
+                <div className="level-connector"></div>
+                <Button
+                    type="ToolboxLevelButton"
+                    s_ButtonState="Out"
+                    inColor="#060841"
+                    outColor="#060841"
+                    hoverColor="#0000DC"
+                    text={4}
+                    hoverEffect="fill"
+                    onClick={level4OnClick}>
+                </Button>
+                <div className="level-connector"></div>
+                <Button
+                    type="ToolboxLevelButton"
+                    s_ButtonState="Out"
+                    inColor="#060841"
+                    outColor="#060841"
+                    hoverColor="#0000DC"
+                    text={5}
+                    hoverEffect="fill"
+                    onClick={level5OnClick}>
+                </Button>
             </div>
-            <div className="connector">
-                <div id="connector-bottom">
-
-                </div>
-                <div id="connector-middle">
-
-                </div>
-                <div id="connector-top">
-
-                </div>
+            <div className="connector" ref={Connector}>
+                <div id="connector-bottom"/>
+                <div id="connector-middle"/>
+                <div id="connector-top"/>
             </div>
         </div >
     )
