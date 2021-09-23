b9a5606be677f39b01175fd84f5afadd77b80a20
Michael Nimdea-Gyan Adu <michaelngadu@gmail.com>
2021-09-23 11:50:30 +0000

    workspace

---
+++ react-side/src/components/WorkspaceDiv/WorkspaceDiv.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './WorkspaceDiv.scss'
+
+function WorkspaceDiv(props) {
+    return (
+            <div className="workspace">
+            </div>
+
+    )
+}
+
+WorkspaceDiv.propTypes = {
+
+}
+
+export default WorkspaceDiv
+
