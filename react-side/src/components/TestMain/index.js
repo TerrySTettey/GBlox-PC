39bd20b4fba6381624b3ad49fbf6d5378109b7b9
Terry Tettey <35869292+MonoTerry@users.noreply.github.com>
2021-09-27 16:56:48 +0000

    Overlay Border working properly

--- react-side/src/components/TestMain/index.js
+++ react-side/src/components/TestMain/index.js
@@ -8,7 +8,7 @@ const TestMain = () => {
     var AllScrolls = document.getElementsByClassName("blocklyScrollbarHandle");
     var i = 0;
     for(i = 0; i < AllScrolls.length; i++){
-        AllScrolls[i].style.display = "none";
+        AllScrolls[i].style.display = "none !important";
     }
     return (
         <div>
