0857b9bc1c8a67816c4a7bc3a307f1b04e417bce
Michael Nimdea-Gyan Adu <michaelngadu@gmail.com>
2021-10-29 14:09:13 +0000

    removed a console log

--- react-side/src/customblocks/compiler/arduino_core.js
+++ react-side/src/customblocks/compiler/arduino_core.js
@@ -208,7 +208,6 @@ Blockly.JavaScript['m_mainloop'] = function (block) {
     block.setMovable(false);
 
     var statements_mainloop = Blockly.JavaScript.statementToCode(block, 'mainLoop');
-    console.log(createdVariables)
     if (createdVariables.length != 0) {
         variables_set = createdVariables;
         varcheck = createdVariables
