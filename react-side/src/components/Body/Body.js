7df8bd5dbcb92f33333d52e3d2302e49f5a83c22
Michael Nimdea-Gyan Adu <michaelngadu@gmail.com>
2021-11-01 10:55:05 +0000

    Removed Alert

--- react-side/src/components/Body/Body.js
+++ react-side/src/components/Body/Body.js
@@ -247,7 +247,6 @@ const Body = (props) => {
                 break;
             default:
                 console.log(upload_status);
-                alert(upload_status);
                 setInUpload(false);
                 setProgressValue(0);
                 break;
