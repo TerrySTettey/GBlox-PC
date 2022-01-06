194fb08cc168dd68dbb8b81cefb42ae9af3cacb5
Terry Tettey <seltettey@gmail.com>
2022-01-06 11:15:32 +0000

    Mingo Toolbox Updated

--- react-side/src/components/NewDeviceManager/index.js
+++ react-side/src/components/NewDeviceManager/index.js
@@ -76,7 +76,7 @@ function Index(props) {
                 <div className="initial-text">Select Your Device</div>
                 <div className="Device-Items">
                     {add_device(svg_dictionary.devices.Arduino_Uno_SVG, "Arduino Uno")}
-                    {add_device(svg_dictionary.devices.mello, "Mello")}
+                    {/* {add_device(svg_dictionary.devices.mello, "Mello")} */}
                     {add_device(svg_dictionary.devices.mingo, "Mingo")}
                 </div>
             </div>
--- react-side/src/deviceDef/device_list.js
+++ react-side/src/deviceDef/device_list.js
@@ -608,7 +608,329 @@ const Mingo_Bot = new device(
   "Mingo",
   "Arduino",
   "Arduino Nano",
-  [defaults + `
+  [`<xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: none">
+  <category css-icon="customIcon fa fa-cog" name="Actuators" colour="#05386B">
+          <block type="mingo_motor_move_indef">
+              <field name="direction">forward</field>
+              <field name="speed">50</field>
+          </block>
+          <block type="mingo_motor_single_move_indef">
+              <field name="motorselect">lm</field>
+              <field name="direction">forward</field>
+              <field name="speed">50</field>
+          </block>
+          <block type="mingo_motor_stop_indef">
+              <field name="motor">all</field>
+          </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="LEDs">
+          <block type="mingo_led_range">
+              <field name="ledSelect">all</field>
+              <field name="port">1</field>
+              <value name="red">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+              <value name="green">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+              <value name="blue">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+          </block>
+          <block type="mingo_led_definite">
+              <field name="ledSelect">all</field>
+              <field name="port">1</field>
+              <field name="colorSelect">Red</field>
+          </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="Sound">
+      <block type="mingo_sound_play">
+          <field name="note">1047</field>
+      </block>
+      <block type="mingo_sound_play_timed">
+          <field name="note">1047</field>
+          <value name="Buzzer Time">
+              <block type="math_number">
+                  <field name="NUM">0</field>
+              </block>
+          </value>
+      </block>
+      <block type="mingo_sound_stop"></block>
+      <block type="mingo_sound_play_song">
+          <field name="song">merry</field>
+      </block>
+  </category>
+  </xml>`, `<xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: none">
+  <category name="Loops">
+      <block type="delay_core">
+      <value name="seconds">
+      <shadow type="math_number">
+          <field name="seconds">1</field>
+      </shadow>
+  </value>
+      </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="Actuators" colour="#05386B">
+          <block type="mingo_motor_move_indef">
+              <field name="direction">forward</field>
+              <field name="speed">50</field>
+          </block>
+          <block type="mingo_motor_single_move_indef">
+              <field name="motorselect">lm</field>
+              <field name="direction">forward</field>
+              <field name="speed">50</field>
+          </block>
+          <block type="mingo_motor_stop_indef">
+              <field name="motor">all</field>
+          </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="LEDs">
+          <block type="mingo_led_range">
+              <field name="ledSelect">all</field>
+              <field name="port">1</field>
+              <value name="red">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+              <value name="green">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+              <value name="blue">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+          </block>
+          <block type="mingo_led_definite">
+              <field name="ledSelect">all</field>
+              <field name="port">1</field>
+              <field name="colorSelect">Red</field>
+          </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="Sound">
+      <block type="mingo_sound_play">
+          <field name="note">1047</field>
+      </block>
+      <block type="mingo_sound_play_timed">
+          <field name="note">1047</field>
+          <value name="Buzzer Time">
+              <block type="math_number">
+                  <field name="NUM">0</field>
+              </block>
+          </value>
+      </block>
+      <block type="mingo_sound_stop"></block>
+      <block type="mingo_sound_play_song">
+          <field name="song">merry</field>
+      </block>
+  </category>
+  </xml>`, `<xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: none">
+  <category name="Loops">
+      <block type="for_loop">
+      </block>
+      <block type="controls_whileUntil">
+          <field name="MODE">WHILE</field>
+      </block>
+      <block type="delay_core">
+      <value name="seconds">
+      <shadow type="math_number">
+          <field name="seconds">1</field>
+      </shadow>
+  </value>
+      </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="Actuators" colour="#05386B">
+          <block type="mingo_motor_move_indef">
+              <field name="direction">forward</field>
+              <field name="speed">50</field>
+          </block>
+          <block type="mingo_motor_single_move_indef">
+              <field name="motorselect">lm</field>
+              <field name="direction">forward</field>
+              <field name="speed">50</field>
+          </block>
+          <block type="mingo_motor_stop_indef">
+              <field name="motor">all</field>
+          </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="LEDs">
+          <block type="mingo_led_range">
+              <field name="ledSelect">all</field>
+              <field name="port">1</field>
+              <value name="red">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+              <value name="green">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+              <value name="blue">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+          </block>
+          <block type="mingo_led_definite">
+              <field name="ledSelect">all</field>
+              <field name="port">1</field>
+              <field name="colorSelect">Red</field>
+          </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="Sound">
+      <block type="mingo_sound_play">
+          <field name="note">1047</field>
+      </block>
+      <block type="mingo_sound_play_timed">
+          <field name="note">1047</field>
+          <value name="Buzzer Time">
+              <block type="math_number">
+                  <field name="NUM">0</field>
+              </block>
+          </value>
+      </block>
+      <block type="mingo_sound_stop"></block>
+      <block type="mingo_sound_play_song">
+          <field name="song">merry</field>
+      </block>
+  </category>
+  </xml>`, `<xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: none">
+  <category css-icon="customIcon fa fa-cog" name="Logic">
+      <block type="controls_if"></block>
+      <block type="logic_compare">
+          <field name="OP">EQ</field>
+      </block>
+      <block type="logic_operation">
+          <field name="OP">AND</field>
+      </block>
+      <block type="logic_negate"></block>
+      <block type="logic_boolean">
+          <field name="BOOL">TRUE</field>
+      </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="Math">
+      <block type="math_number">
+          <field name="NUM">0</field>
+      </block>
+      <block type="math_arithmetic">
+          <field name="OP">ADD</field>
+          <value name="A">
+              <shadow type="math_number">
+                  <field name="NUM">1</field>
+              </shadow>
+          </value>
+          <value name="B">
+              <shadow type="math_number">
+                  <field name="NUM">1</field>
+              </shadow>
+          </value>
+      </block>
+      <block type="math_number_property">
+          <mutation divisor_input="false"></mutation>
+          <field name="PROPERTY">EVEN</field>
+          <value name="NUMBER_TO_CHECK">
+              <shadow type="math_number">
+                  <field name="NUM">0</field>
+              </shadow>
+          </value>
+      </block>
+  </category>
+  <category name="Loops">
+      <block type="for_loop">
+      </block>
+      <block type="controls_whileUntil">
+          <field name="MODE">WHILE</field>
+      </block>
+      <block type="delay_core">
+        <value name="seconds">
+          <shadow type="math_number">
+            <field name="seconds">1</field>
+          </shadow>
+        </value>
+      </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="Actuators" colour="#05386B">
+          <block type="mingo_motor_move_indef">
+              <field name="direction">forward</field>
+              <field name="speed">50</field>
+          </block>
+          <block type="mingo_motor_single_move_indef">
+              <field name="motorselect">lm</field>
+              <field name="direction">forward</field>
+              <field name="speed">50</field>
+          </block>
+          <block type="mingo_motor_stop_indef">
+              <field name="motor">all</field>
+          </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="Sensors">
+          <block type="mingo_ultrasonic_sensor">
+              <field name="port">1</field>
+          </block>
+          <block type="mingo_light_read">
+              <field name="sensor">left</field>
+              <field name="port">3</field>
+          </block>
+          <block type="mingo_line_read">
+              <field name="sensor">left</field>
+              <field name="port">1</field>
+          </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="LEDs">
+          <block type="mingo_led_range">
+              <field name="ledSelect">all</field>
+              <field name="port">1</field>
+              <value name="red">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+              <value name="green">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+              <value name="blue">
+                  <block type="math_number">
+                      <field name="NUM"></field>
+                  </block>
+              </value>
+          </block>
+          <block type="mingo_led_definite">
+              <field name="ledSelect">all</field>
+              <field name="port">1</field>
+              <field name="colorSelect">Red</field>
+          </block>
+  </category>
+  <category css-icon="customIcon fa fa-cog" name="Sound">
+      <block type="mingo_sound_play">
+          <field name="note">1047</field>
+      </block>
+      <block type="mingo_sound_play_timed">
+          <field name="note">1047</field>
+          <value name="Buzzer Time">
+              <block type="math_number">
+                  <field name="NUM">0</field>
+              </block>
+          </value>
+      </block>
+      <block type="mingo_sound_stop"></block>
+      <block type="mingo_sound_play_song">
+          <field name="song">merry</field>
+      </block>
+  </category>
+  </xml>`, defaults + `
   <category css-icon="customIcon fa fa-cog" name="Actuators" colour="#05386B">
           <block type="mingo_motor_move_indef">
               <field name="direction">forward</field>
